6dc7f8f834bf1e61aeb5b36798deab1403ac8fd8
Michael J <30031129@cc.peralta.edu>
2024-09-15 17:55:15 -0700

    added commission cost to print statemnt

--- Commission.cpp
+++ Commission.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(){
     double sales;
     double commission;
+    
     cout << "how much money have you made in sales?" << endl;
     cin >> sales;
+
     if(sales > 15'000){
         commission = 0.2;
     }else if (sales >= 10'000){
@@ -15,5 +17,7 @@ int main(){
         commission = 0.1;
     }
     
+    cout << "your commission is: " << sales * commission;
+
     return 0;
 }
